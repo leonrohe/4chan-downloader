64f665159e0a9db37a6b1c781425e364df4b81d1
leonrohe <89611453+leonrohe@users.noreply.github.com>
2022-09-12 16:52:03 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -5,48 +5,50 @@ from bs4 import BeautifulSoup
 
 url = input("Enter the URL of the 4chan thread. \n")
 
-## get response from url
+# get response from url
 r = requests.get(url)
 
-## create soup out of html text
+# create soup out of html text
 soup = BeautifulSoup(r.text, "html.parser")
 
-## get current working directory
+# get current working directory
 cwd = os.getcwd()
 
 folder_title = ""
 
-## find thread subject, if exists
+# find thread subject, if exists
 thread_subject = soup.find(class_="subject").text
 
-## remove every illegal character from thread_subject
-illegal_characters = ['\', '/', ':', '?', '"', '<', '>', '|']
+# remove every illegal character from thread_subject
+illegal_characters = ['/', ':', '"', '?', '<', '>', '|']
 for character in illegal_characters:
     thread_subject = thread_subject.replace(character, "")
 
-## set name of folder to create
+# set name of folder to create
 if thread_subject != "":
     folder_title = thread_subject
 else:
     folder_title = str(int(time.time()))
     print("<WARNING> Could not parse Thread Subject. Temporary folder name was given")
 
-## create folder path
+# create folder path
 dir = os.path.join(cwd, folder_title)
 
-## create folder
+# create folder
 try:
     os.mkdir(dir)
 except:
-    print("<WARNING> Could not create folder: " + dir + " .Maybe it already exists?")
+    print("<WARNING> Could not create folder: " +
+          dir + " .Maybe it already exists?")
 
-## get all media from thread
+# get all media from thread
 media_all = soup.find_all(class_="fileThumb")
 file_count = len(media_all)
 
-print("Found " + str(file_count) + " downloadable media files. Starting to download ...")
+print("Found " + str(file_count) +
+      " downloadable media files. Starting to download ...")
 
-## loop through all media files and save them to folder
+# loop through all media files and save them to folder
 z = 0
 for media in media_all:
 
